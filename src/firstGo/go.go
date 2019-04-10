7dc30cc6a0233969f4afd61b5361da01e023343a
huynq <huynq@tmh-techlab.vn>
2019-04-10 16:15:35 +0700

    qrcode

--- src/firstGo/go.go
+++ src/firstGo/go.go
@@ -1,11 +1,14 @@
 package main
 import(
+	"io"
+	"os"
 	"fmt"
 	"net/http"
 	"encoding/json"
 	"time"
 	"strconv"
 	"log"
+	"github.com/skip2/go-qrcode"
 )
 type User struct {
 	Username string
@@ -18,10 +21,38 @@ func main(){
 	mux.HandleFunc("/", index)
 	mux.HandleFunc("/echo", post)
 	mux.HandleFunc("/redirect", redirect)
+	mux.HandleFunc("/qrcode", qr_code)
 	http.ListenAndServe(":9999", mux)
 }
 func index(w http.ResponseWriter, r *http.Request){
 	fmt.Fprintf(w, "Hello, Line")
+}
+func qr_code(w http.ResponseWriter, r *http.Request){
+	if r.Method != "POST"{
+		fmt.Fprintf(w, "Only accept POST request")
+	}else{
+		userName := r.FormValue("Username")
+		createAt := time.Now().Local()
+		isMale, err := strconv.ParseBool(r.FormValue("IsMale"))
+		if err != nil{
+			fmt.Println("Something wrong with key IsMale")
+		}
+		id, err := strconv.Atoi(r.FormValue("Id"))
+		if err != nil{
+			fmt.Println("Something wrong with key Id")
+		}
+		user := User{
+			Username : userName,
+			Id : id,
+			IsMale: isMale,
+			CreatedAt: createAt,
+		} 	
+		userJson, err := json.Marshal(user)
+		err = qrcode.WriteFile(string(userJson), qrcode.Medium, 256, r.FormValue("Id")+createAt.String()+".png")
+	    img, err := os.Open(userName+"-"+r.FormValue("Id")+".png")
+	    w.Header().Set("Content-Type", "image/jpeg")
+	    io.Copy(w, img)
+	}
 }
 func post(w http.ResponseWriter, r *http.Request){
 	if r.Method != "POST"{
